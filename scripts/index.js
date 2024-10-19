914dbd1626288877449a514125bac2a86fb83a40
Ilya Gusinsky <gusinskyilya@gmail.com>
2024-10-19 16:16:31 +0300

    6 cards and delete button v.1

--- scripts/index.js
+++ scripts/index.js
@@ -1,9 +1,31 @@
 // @todo: Темплейт карточки
+const cardTemplate = document.querySelector('#card-template').content; 
 
 // @todo: DOM узлы
+const placesList = document.querySelector('.places__list');
 
-// @todo: Функция создания карточки
+// // // @todo: Функция создания карточки
+const addCard =  function (cardData, delButton) {
+  const cardElement = cardTemplate.querySelector('.places__item').cloneNode(true);
+  cardElement.querySelector('.card__title').textContent = cardData.name;
+
+  const Image = cardElement.querySelector('.card__image');
+  Image.src = cardData.link;
+  Image.alt = cardData.name;
+
+  const deleteButton = cardElement.querySelector('.card__delete-button');
+  deleteButton.addEventListener('click', () => delButton(cardElement));
+
+  return cardElement;
+}
 
 // @todo: Функция удаления карточки
+function delButton(cardElement) {
+    cardElement.remove();
+  }
 
 // @todo: Вывести карточки на страницу
+initialCards.forEach((cardData) => {
+  const cardElement = addCard(cardData, delButton);
+  placesList.append(cardElement);
+});
