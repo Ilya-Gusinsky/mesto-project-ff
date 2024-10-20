47246d92176162f5ca5e3ab34db9f1c014d5af21
Ilya Gusinsky <gusinskyilya@gmail.com>
2024-10-20 12:58:00 +0300

    fix names of variables

--- scripts/index.js
+++ scripts/index.js
@@ -5,13 +5,13 @@ const cardTemplate = document.querySelector('#card-template').content;
 const placesList = document.querySelector('.places__list');
 
 // // // @todo: Функция создания карточки
-const addCard =  function (cardData, delButton) {
+const createCard =  function (cardData, delButton) {
   const cardElement = cardTemplate.querySelector('.places__item').cloneNode(true);
   cardElement.querySelector('.card__title').textContent = cardData.name;
 
-  const Image = cardElement.querySelector('.card__image');
-  Image.src = cardData.link;
-  Image.alt = cardData.name;
+  const image = cardElement.querySelector('.card__image');
+  image.src = cardData.link;
+  image.alt = cardData.name;
 
   const deleteButton = cardElement.querySelector('.card__delete-button');
   deleteButton.addEventListener('click', () => delButton(cardElement));
@@ -26,6 +26,6 @@ function delButton(cardElement) {
 
 // @todo: Вывести карточки на страницу
 initialCards.forEach((cardData) => {
-  const cardElement = addCard(cardData, delButton);
+  const cardElement = createCard(cardData, delButton);
   placesList.append(cardElement);
 });
